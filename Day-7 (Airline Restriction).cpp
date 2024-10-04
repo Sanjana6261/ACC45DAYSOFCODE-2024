902ec03e5d9265440807c28c1cfdeee9ff55cdbc
Sanjana6261 <158602083+Sanjana6261@users.noreply.github.com>
2024-10-04 20:59:23 +0530

    Add files via upload

---
+++ Day-7 (Airline Restriction).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+    int A, B, C, D, E
+    If (A + B <= D && C <= E) || (A + C <= D && B <= E) || (B + C <= D && A <= E){
+	
+        cout<< "YES"<<endl;
+    }
+    Else{}
+        cout<< "NO" <<endl;
+    }
+    return 0;
+}
