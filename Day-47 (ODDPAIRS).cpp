df94fbddd713a00553d6b7b078422170d79fc232
Sanjana6261 <158602083+Sanjana6261@users.noreply.github.com>
2024-11-18 20:05:11 +0530

    Add files via upload

---
+++ Day-47 (ODDPAIRS).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n;
+	    cin>>n;
+	    int cnt=0;
+	   // for(int i=1;i<=n;i++){
+	   // }
+	   cout<<(n*n)/2<<endl;
+	    
+	    
+	       // for(int j=1;j<=n;j++){
+	          //  if((i+j)%2!=0){
+	               // cnt++;
+	          //  }
+	       // }
+	  //  }
+	           // cout<<cnt<<endl;
+	        
+	    
+	}
+	return 0;
+}
